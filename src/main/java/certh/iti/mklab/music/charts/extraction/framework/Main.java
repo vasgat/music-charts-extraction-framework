a40bc99016ea6beafdf88ef1356ac325ff89a3da
vasgat <vasgat@gmail.com>
2019-02-12 14:34:52 +0200

    Refactor

--- src/main/java/certh/iti/mklab/music/charts/extraction/framework/Main.java
+++ src/main/java/certh/iti/mklab/music/charts/extraction/framework/Main.java
@@ -8,7 +8,7 @@ package certh.iti.mklab.music.charts.extraction.framework;
 import certh.iti.mklab.music.charts.extraction.framework.chartUtils.Chart;
 import certh.iti.mklab.music.charts.extraction.framework.chartUtils.ChartEntryType;
 import certh.iti.mklab.music.charts.extraction.framework.extractors.ChartExtractor;
-import certh.iti.mklab.music.charts.extraction.framework.stringUtils.DateFormatter;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -20,23 +20,24 @@ public class Main {
 
     public static void main(String[] args)
             throws URISyntaxException, IOException, InterruptedException {
-        
-        Chart top40 = new ChartExtractor.Builder("http://top40-charts.com/chart.php?cid=44", false)
-                .custom_country("global").type(ChartEntryType.TRACK)
-                .chart_name("font.biggerblue")
-                .date("select[name=date] option[selected]")
-                .table_rows("tr.latc_song")
-                .artist("td:nth-child(4) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3) > a:nth-child(2)")
-                .title("td:nth-child(4) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3) > div:nth-child(1)")
+
+        Chart kworb = new ChartExtractor.Builder("http://kworb.net/youtube/insights/se_daily.html", false)
+                .custom_country("global")
+                .chart_id("123456")
+                .type(ChartEntryType.VIDEO)
+                .chart_name("span.pagetitle")
+                .date("span.pagetitle")
+                .table_rows("#dailytable > tbody tr")
+                .title("td:nth-child(3)")
                 .position("td:nth-child(1)")
-                .additionalInfo("youtube_id", "td:nth-child(4) td.videolink", "style")
+                .additionalInfo("streams_change", "td:nth-child(5)")
+                .additionalInfo("streams", "td:nth-child(4)")
                 .build();
 
-        String[] dateFields = top40.getDate().split("\\-");
-        top40.setDate(DateFormatter.transformEndDateToDateSpan(dateFields[0], dateFields[1], dateFields[2]));
-        top40.process_entry("youtube_id", ".*img.youtube.com/vi/", "");
-        top40.process_entry("youtube_id", "/.*", "");
+        String[] date = kworb.getDate().replaceAll(".* - ", "").replaceAll("\\|.*", "").trim().split("/");
+        kworb.setDate(date[2] + "/" + date[1] + "/" + date[0]);
+        kworb.setName(kworb.getName().replaceAll(" - .*", ""));
 
-        System.out.println(top40.toJSON().toJson());
+        System.out.println(kworb.toJSON().toJson());
     }
 }
